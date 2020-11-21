44d7e98f4cbb1000c5c9938ff973474889300942
tardigrada777 <ntwrite@gmail.com>
2020-11-21 21:16:23 +0300

    tests(sticky): improve sticky unit tests

--- src/components/sticky.ts
+++ src/components/sticky.ts
@@ -9,25 +9,28 @@ export const sticky = defineComponent({
   props: {
     to: {
       type: String,
-      required: true,
       default: () => 'top',
-      validator: (value: string) => stickyDirections.includes(value),
+      validator: (value: string) => stickyDirections.includes(value)
     },
     offset: {
       type: Number,
-      default: () => 0,
+      default: () => 0
     }
   },
   render() {
     const { to, offset } = this.$props;
     const styles = getCustomStyles(this.$attrs);
 
-    return h('div', {
-      className: 'l-sticky',
-      style: {
-        ...styles,
-        [to]: px(offset),
-      }
-    }, [ slot(this.$slots.default) ]);
+    return h(
+      'div',
+      {
+        className: 'l-sticky',
+        style: {
+          ...styles,
+          [to]: px(offset)
+        }
+      },
+      [slot(this.$slots.default)]
+    );
   }
 });
--- tests/unit/sticky.spec.ts
+++ tests/unit/sticky.spec.ts
@@ -7,4 +7,48 @@ describe('l-sticky', () => {
     expect(wrapper.exists()).toBeTruthy();
     expect(wrapper.find('.l-sticky').exists()).toBeTruthy();
   });
+
+  it('renders props.offset by default with 0px', () => {
+    const wrapper = shallowMount(sticky);
+    expect(wrapper.find<HTMLElement>('.l-sticky').element.style.top).toEqual(
+      '0px'
+    );
+  });
+
+  it('renders props.offset correctly', () => {
+    const offset = 15;
+
+    const wrapper = shallowMount(sticky, {
+      props: {
+        offset
+      }
+    });
+    expect(wrapper.find<HTMLElement>('.l-sticky').element.style.top).toEqual(
+      `${offset}px`
+    );
+  });
+
+  it('renders props.to correctly', () => {
+    const wrapper = shallowMount(sticky, {
+      props: {
+        to: 'left'
+      }
+    });
+    expect(wrapper.find<HTMLElement>('.l-sticky').element.style.left).toEqual(
+      '0px'
+    );
+  });
+
+  it('renders props.to and props.offset together correctly', () => {
+    const offset = 77;
+    const wrapper = shallowMount(sticky, {
+      props: {
+        to: 'bottom',
+        offset
+      }
+    });
+    expect(wrapper.find<HTMLElement>('.l-sticky').element.style.bottom).toEqual(
+      `${77}px`
+    );
+  });
 });
